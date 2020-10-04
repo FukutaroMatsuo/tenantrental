c93fe048cf3b9bf276fa6392be617e3e7346eac1
FukutaroMatsuo <54502796+FukutaroMatsuo@users.noreply.github.com>
2020-10-04 13:22:32 +0900

    rspec 関連の修正

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,16 +1,4 @@
-Capybara.register_driver :chrome_headless do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
-
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--window-size=1400,1400')
-  
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-end
-
-# Capybara.javascript_driver = :chrome_headless
-Capybara.default_driver = :rack_test
+Capybara.javascript_driver = :chrome_headless
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
@@ -18,14 +6,6 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :system, js: true) do
-    if ENV["SELENIUM_DRIVER_URL"].present?
-      driven_by :selenium, using: :chrome, options: {
-        browser: :remote,
-        url: ENV.fetch("SELENIUM_DRIVER_URL"),
-        desired_capabilities: :chrome
-      }
-    else
-      driven_by :selenium_chrome_headless
-    end
+    driven_by :chrome_headless
   end
 end
