0129ca199002c4dd543e1159896492e96159cad3
FukutaroMatsuo <54502796+FukutaroMatsuo@users.noreply.github.com>
2020-10-09 21:19:54 +0900

    heroku deploy 仮完成

--- spec/requests/users_signup_spec.rb
+++ spec/requests/users_signup_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 RSpec.describe "ユーザー登録", type: :request do
-  # before do
-  #   get signup_path
-  # end
+  before do
+    get signup_path
+  end
 
-  # it "正常なレスポンスを返すこと" do
-  #   expect(response).to be_success
-  #   expect(response).to have_http_status "200"
-  # end
+  it "正常なレスポンスを返すこと" do
+    expect(response).to be_success
+    expect(response).to have_http_status "200"
+  end
 
   # it "有効なユーザーで登録" do
   #   expect {
